660038723d07bef90a51300b2ae823c2c64c87d1
DevvyE_yo <DevEchoX@gmail.com>
2026-08-12 20:59:49 -0400

    Update celsius.config.js

--- celsius/celsius.config.js
+++ celsius/celsius.config.js
@@ -1,7 +1,7 @@
 self.__celsius$config = {
     prefix: '/celsius/go/',
     // Replace this with your actual Vercel project URL!! very important
-    bare: '/api/bare', 
+    bare: 'https://celsius-engine.vercel.app/api/bare', 
     encodeUrl: url => btoa(url).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, ''),
     decodeUrl: url => {
         let str = url.replace(/-/g, '+').replace(/_/g, '/');
