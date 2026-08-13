e5793912222f8dec938547f6a8887d017de97504
DevvyE_yo <DevEchoX@gmail.com>
2026-08-12 21:18:31 -0400

    Update celsius.sw.js

--- celsius/celsius.sw.js
+++ celsius/celsius.sw.js
@@ -1,7 +1,8 @@
 importScripts('celsius.config.js');
 importScripts('celsius.bundle.js');
 
-const engine = new self.CelsiusRouter(self.__celsius$config);
+// Option 1 Fix: 'self.' is removed from CelsiusRouter
+const engine = new CelsiusRouter(self.__celsius$config);
 
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(clients.claim()));
